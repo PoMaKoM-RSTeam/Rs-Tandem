fc4bc6fd492f0477d0e51a132a3772fe18584b72
Alexandr Zhdanko <lex.zhdanko@gmail.com>
2026-02-16 18:06:00 +0400

    feat(ui): add button component, SCSS variables and fonts
    - Created TndmButtonComponent with
    - Added global SCSS variables for colors, typography, and sizes
    - Connected project fonts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AngularSvgIconModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
