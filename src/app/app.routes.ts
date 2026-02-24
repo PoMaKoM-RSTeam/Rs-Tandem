2e25a3326debb943d56de861cd72c897cf44a0f4
Mark Pribylnov <mark.pribylnov@gmail.com>
2026-02-24 16:18:24 +0300

    feat: add initial UI for Async Sorter (event loop game) without logic (#14)
    * chore: add diary note
    * feat: add game structure in HTML + route
    * feat: add basic UI
    * chore: format with prettier
    * chore: add `code-blocks-list.html` to `.prettierignore` to prevent indentation conflict

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TndmAsyncSorter } from './pages/games/async-sorter/async-sorter';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'async-sorter',
+    component: TndmAsyncSorter,
+  },
+];
