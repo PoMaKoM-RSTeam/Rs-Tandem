acf8af421b35995a6273dadf4e1a15988718bb6c
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-02-27 02:12:25 +0300

    feat: add index.ts file

---
+++ src/app/auth/index.ts
@@ -0,0 +1,5 @@
+export { AUTH_ROUTES, AUTH_ROUTES_SEGMENTS } from './constants/router';
+export { authRoutes } from './tndm-auth-routes';
+export { tndmAuthInterceptor } from './tndm-auth-interceptor';
+export { tndmAuthGuard } from './tndm-auth-guard';
+export { TndmAuthService } from './tndm-auth-service';
