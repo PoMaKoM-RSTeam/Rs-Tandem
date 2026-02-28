ec08c660fa89e0ec1704ca093ce2965187cfd7b4
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-02-28 04:44:02 +0300

    fix: remove unnecessary guard

--- src/app/auth/tndm-auth-routes.ts
+++ src/app/auth/tndm-auth-routes.ts
@@ -1,27 +1,23 @@
 import { Routes } from '@angular/router';
-import { AUTH_ROUTES_SEGMENTS, tndmAuthGuard } from '@auth';
+import { AUTH_ROUTES_SEGMENTS } from '@auth';
 
 export const authRoutes: Routes = [
   { path: '', redirectTo: AUTH_ROUTES_SEGMENTS.LOGIN, pathMatch: 'full' },
   {
     path: AUTH_ROUTES_SEGMENTS.LOGIN,
     loadComponent: () => import('./forms/tndm-login-form/tndm-login-form').then(m => m.TndmLoginForm),
-    canActivate: [tndmAuthGuard],
   },
   {
     path: AUTH_ROUTES_SEGMENTS.REGISTER,
     loadComponent: () => import('./forms/tndm-register-form/tndm-register-form').then(m => m.TndmRegisterForm),
-    canActivate: [tndmAuthGuard],
   },
   {
     path: AUTH_ROUTES_SEGMENTS.FORGOT_PASSWORD,
     loadComponent: () =>
       import('./forms/tndm-forgot-password-form/tndm-forgot-password-form').then(m => m.TndmForgotPasswordForm),
-    canActivate: [tndmAuthGuard],
   },
   {
     path: AUTH_ROUTES_SEGMENTS.UPDATE_PASSWORD,
     loadComponent: () => import('./forms/tndm-update-password/tndm-update-password').then(m => m.TndmUpdatePassword),
-    canActivate: [tndmAuthGuard],
   },
 ];
