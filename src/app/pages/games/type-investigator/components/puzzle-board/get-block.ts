310cc230d7d09cbf828ad07dab968cd4e9a91f65
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-03-03 07:36:41 +0500

    feat: add block checker by id

---
+++ src/app/pages/games/type-investigator/components/puzzle-board/get-block.ts
@@ -0,0 +1,8 @@
+import { CodeBlock, Puzzle } from '../../models/puzzle.model';
+
+export function getBlock(puzzle: Puzzle, id: string | null): CodeBlock | undefined {
+  if (!id) {
+    return undefined;
+  }
+  return puzzle.availableBlocks.find(b => b.id === id);
+}
