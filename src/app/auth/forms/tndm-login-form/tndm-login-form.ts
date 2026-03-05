b8f5841c22d60707155815aea6effda4ef298e09
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-03-05 23:42:41 +0300

    fix: move buttons configs to ts file to avoid problems with change detections for login

--- src/app/auth/forms/tndm-login-form/tndm-login-form.ts
+++ src/app/auth/forms/tndm-login-form/tndm-login-form.ts
@@ -1,7 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, Signal } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TndmInputComponent } from '../../../shared/ui/tndm-input-component/tndm-input-component';
-import { TndmButtonComponent } from '../../../shared/ui/tndm-button-component/tndm-button-component';
+import { ButtonConfig, TndmButton } from '../../../shared/ui/tndm-button/tndm-button';
 import { RouterLink } from '@angular/router';
 import { AUTH_ROUTES } from '../../';
 import { TndmAuthFormCore } from '../../tndm-auth-form-core/tndm-auth-form-core';
@@ -10,12 +10,37 @@ import { FormField } from '../../enums/auth-field-types';
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'tndm-tndm-login-form',
-  imports: [ReactiveFormsModule, TndmInputComponent, TndmButtonComponent, RouterLink],
+  imports: [ReactiveFormsModule, TndmInputComponent, TndmButton, RouterLink],
   templateUrl: './tndm-login-form.html',
   styleUrl: './tndm-login-form.scss',
 })
 export class TndmLoginForm extends TndmAuthFormCore {
   protected readonly toForgotPasswordPath: string = AUTH_ROUTES.FORGOT_PASSWORD;
+
+  protected readonly signInBtnConfig: Signal<ButtonConfig> = computed(() => ({
+    label: 'Sign-In',
+    type: 'submit',
+    isDisabled: !this.canSubmit(),
+  }));
+
+  protected readonly signWithGoogleBtnConfig: Signal<ButtonConfig> = computed(() => ({
+    icon: 'google',
+    variant: 'black',
+    label: 'Sign-In with Google',
+    isDisabled: this.isLoading(),
+  }));
+  protected readonly signWithGithubBtnConfig: Signal<ButtonConfig> = computed(() => ({
+    icon: 'github',
+    variant: 'black',
+    label: 'Sign-In with Github',
+    isDisabled: this.isLoading(),
+  }));
+
+  protected readonly toRegisterBtnConfig: Signal<ButtonConfig> = computed(() => ({
+    variant: 'secondary',
+    label: 'Sign-up',
+    isDisabled: this.isLoading(),
+  }));
 
   constructor() {
     super();
@@ -27,6 +52,9 @@ export class TndmLoginForm extends TndmAuthFormCore {
   }
 
   protected override async handleSubmit(): Promise<void> {
-    await this.authService.login(this.emailControl.value, this.passwordControl.value);
+    const user = await this.authService.login(this.emailControl.value, this.passwordControl.value);
+    if (user) {
+      this.navigateToMain();
+    }
   }
 }
