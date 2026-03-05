f910f8b4ff53d9bbab1c03a695e799b437be1bab
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-03-05 23:41:30 +0300

    fix: move buttons configs to ts file to avoid problems with change detections for forgot password

--- src/app/auth/forms/tndm-forgot-password-form/tndm-forgot-password-form.ts
+++ src/app/auth/forms/tndm-forgot-password-form/tndm-forgot-password-form.ts
@@ -1,18 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, Signal } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TndmInputComponent } from '../../../shared/ui/tndm-input-component/tndm-input-component';
-import { TndmButtonComponent } from '../../../shared/ui/tndm-button-component/tndm-button-component';
+import { ButtonConfig, TndmButton } from '../../../shared/ui/tndm-button/tndm-button';
 import { TndmAuthFormCore } from '../../tndm-auth-form-core/tndm-auth-form-core';
 import { FormField } from '../../enums/auth-field-types';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'tndm-tndm-forgot-password-form',
-  imports: [ReactiveFormsModule, TndmInputComponent, TndmButtonComponent],
+  imports: [ReactiveFormsModule, TndmInputComponent, TndmButton],
   templateUrl: './tndm-forgot-password-form.html',
   styleUrl: './tndm-forgot-password-form.scss',
 })
 export class TndmForgotPasswordForm extends TndmAuthFormCore {
+  protected readonly resetPasswordBtnConfig: Signal<ButtonConfig> = computed<ButtonConfig>(() => ({
+    label: 'Reset Password',
+    isDisabled: !this.canSubmit(),
+    type: 'submit',
+  }));
+
+  protected readonly toLoginBtnConfig: Signal<ButtonConfig> = computed<ButtonConfig>(() => ({
+    variant: 'secondary',
+    label: 'Login',
+    isDisabled: this.isLoading(),
+  }));
+
   constructor() {
     super();
   }
