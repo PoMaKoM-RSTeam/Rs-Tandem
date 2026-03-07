b3670f506a96167fded65ee8450495eeeb359cfc
mark-pribylnov <mark.pribylnov@gmail.com>
2026-03-07 18:50:47 +0300

    feat: add dragging from source list to code-buckets

--- src/app/pages/games/async-sorter/components/code-blocks-list/code-blocks-list.ts
+++ src/app/pages/games/async-sorter/components/code-blocks-list/code-blocks-list.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ChangeDetectionStrategy } from '@angular/core';
 import { TndmCodeBlock } from '../code-block/code-block';
 import codeBlocksJson from './code-blocks.json';
-import { CdkDragDrop, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
+import { CdkDropList } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'tndm-code-blocks-list',
@@ -14,7 +14,7 @@ import { CdkDragDrop, CdkDropList, moveItemInArray } from '@angular/cdk/drag-dro
 export class TndmCodeBlocksList {
   readonly codeBlocks = codeBlocksJson;
 
-  drop(event: CdkDragDrop<string[]>): void {
-    moveItemInArray(this.codeBlocks, event.previousIndex, event.currentIndex);
+  noReturnPredicate(): false {
+    return false;
   }
 }
--- src/app/pages/games/async-sorter/components/task-bucket/task-bucket.ts
+++ src/app/pages/games/async-sorter/components/task-bucket/task-bucket.ts
@@ -1,11 +1,30 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'li[tndm-task-bucket]',
   templateUrl: './task-bucket.html',
   styleUrl: './task-bucket.scss',
+  imports: [CdkDropList],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TndmTaskBucket {
   readonly heading = input.required<string>();
+
+  bucketItems: string[] = [];
+
+  drop(event: CdkDragDrop<string[]>): void {
+    if (event.previousContainer === event.container) {
+      // 1. The user dropped it in the same list they dragged it from (Reordering)
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      // 2. The user dropped it into a different list (Transferring)
+      transferArrayItem(
+        event.previousContainer.data, // The array the item came from
+        event.container.data, // The array the item is going to
+        event.previousIndex, // The index it was at
+        event.currentIndex // The index it was dropped at
+      );
+    }
+  }
 }
