a0f333802c03dd574740a6b4002dcac248558224
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-03-07 05:01:36 +0300

    feat: register new services

--- src/app/auth/index.ts
+++ src/app/auth/index.ts
@@ -3,3 +3,5 @@ export { authRoutes } from './tndm-auth-routes';
 export { tndmAuthInterceptor } from './tndm-auth-interceptor';
 export { tndmAuthGuard } from './tndm-auth-guard';
 export { TndmAuthService } from './tndm-auth-service';
+export { TndmAuthStateStoreService } from './tndm-auth-state-store-service';
+export { TndmAuthApiService } from './tndm-auth-api-service';
