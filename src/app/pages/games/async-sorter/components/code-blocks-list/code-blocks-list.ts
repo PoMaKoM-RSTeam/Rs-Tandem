2f0da8721dabf2f04aa8c4ab3b9574291d990c47
Mark Pribylnov <mark.pribylnov@gmail.com>
2026-03-11 19:34:51 +0300

    feat: restrict dropping to wrong code buckets (#36)
    * refactor: move styles for `task-bucket.scss` and `code-blocks-list.scss` to `shared/_drag-drop.scss`
    * refactor: change styles in  `final-call-stack.scss`
    * feat: restrict dropping to wrong buckets
    * refactor: change hard coded colors to variables
    * refactor: replace a union type with already defined type

--- src/app/pages/games/async-sorter/components/code-blocks-list/code-blocks-list.ts
+++ src/app/pages/games/async-sorter/components/code-blocks-list/code-blocks-list.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { ChangeDetectionStrategy } from '@angular/core';
 import { TndmCodeBlock } from '../code-block/code-block';
-import codeBlocksJson from './code-blocks.json';
-import { CdkDropList } from '@angular/cdk/drag-drop';
+import { codeBlocks } from './code-blocks-data';
+import { CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'tndm-code-blocks-list',
   templateUrl: './code-blocks-list.html',
   styleUrl: './code-blocks-list.scss',
-  imports: [CdkDropList, TndmCodeBlock],
+  imports: [CdkDropList, TndmCodeBlock, CdkDrag],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TndmCodeBlocksList {
-  readonly codeBlocks = codeBlocksJson;
+  readonly codeBlocks = codeBlocks;
 
   noReturnPredicate(): false {
     return false;
--- src/app/pages/games/async-sorter/components/task-bucket/task-bucket.ts
+++ src/app/pages/games/async-sorter/components/task-bucket/task-bucket.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { TndmCodeBlock } from '../code-block/code-block';
+import { CodeBlockData } from '../code-blocks-list/code-blocks-data';
+import { taskType } from '../../shared/types';
 
 @Component({
   selector: 'li[tndm-task-bucket]',
@@ -11,14 +13,18 @@ import { TndmCodeBlock } from '../code-block/code-block';
 })
 export class TndmTaskBucket {
   readonly heading = input.required<string>();
+  readonly taskType = input.required<taskType>();
 
-  bucketItems: string[] = [];
+  bucketItems: CodeBlockData[] = [];
 
-  drop(event: CdkDragDrop<string[]>): void {
+  drop(event: CdkDragDrop<CodeBlockData[]>): void {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
       transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
     }
   }
+
+  taskTypePredicate = (item: CdkDrag<CodeBlockData>): boolean =>
+    item.data.taskType === this.taskType() ? true : false;
 }
