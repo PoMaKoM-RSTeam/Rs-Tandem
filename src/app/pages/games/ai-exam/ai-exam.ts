e46feb978e5c9ccc7c28550870aae2655cbfc679
mark-pribylnov <mark.pribylnov@gmail.com>
2026-03-19 18:52:54 +0300

    feat: connect ollama

--- src/app/pages/games/ai-exam/ai-exam.ts
+++ src/app/pages/games/ai-exam/ai-exam.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TndmButton } from '../../../shared/ui/tndm-button/tndm-button';
+import { OllamaService } from './ollama.service';
 
 @Component({
   selector: 'tndm-ai-exam',
@@ -8,4 +9,10 @@ import { TndmButton } from '../../../shared/ui/tndm-button/tndm-button';
   imports: [TndmButton],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TndmAiExam {}
+export class TndmAiExam {
+  readonly ollama = new OllamaService();
+
+  generateQuestion(): void {
+    this.ollama.ask('3+3');
+  }
+}
