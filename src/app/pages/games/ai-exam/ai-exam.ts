04427e6c173063349cc081d4e74d799f77ebbe7a
mark-pribylnov <mark.pribylnov@gmail.com>
2026-03-19 20:28:16 +0300

    feat: prevent requests to AI while the current one is not finished

--- src/app/pages/games/ai-exam/ai-exam.ts
+++ src/app/pages/games/ai-exam/ai-exam.ts
@@ -12,9 +12,16 @@ import { AiExamOllamaService } from './ollama.service';
 export class TndmAiExam {
   private readonly ollama = inject(AiExamOllamaService);
   readonly currentQuestion = signal('');
+  readonly isLoading = signal(false);
 
   async generateQuestion(): Promise<void> {
-    const answer = await this.ollama.ask('3+3');
-    console.log(answer);
+    if (this.isLoading()) {
+      return;
+    }
+    this.isLoading.set(true);
+
+    const answer = await this.ollama.ask(`3+3 `);
+    this.currentQuestion.set(answer);
+    this.isLoading.set(false);
   }
 }
