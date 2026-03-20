34f50a4f5604294df4531dcf98c4942aced8576a
aliakseidrob <aliakseidrob@gmail.com>
2026-03-20 23:36:54 +0300

    feat: add loading overlay

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TndmToaster } from './shared/ui/tndm-toaster/tndm-toaster';
+import { TndmLoadingOverlay } from './shared/ui/tndm-loading-overlay/tndm-loading-overlay';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TndmToaster],
+  imports: [RouterOutlet, TndmToaster, TndmLoadingOverlay],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
