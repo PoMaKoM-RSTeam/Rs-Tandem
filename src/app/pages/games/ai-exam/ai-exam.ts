e462650124b03b2dff1518e7636188e4112cc1de
mark-pribylnov <mark.pribylnov@gmail.com>
2026-03-23 00:37:14 +0300

    feat: save chat history and pass it with each request

--- src/app/pages/games/ai-exam/ai-exam.ts
+++ src/app/pages/games/ai-exam/ai-exam.ts
@@ -1,6 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { TndmButton } from '../../../shared/ui/tndm-button/tndm-button';
 import { AiExamOllamaService } from './ollama.service';
+import { Message, ROLES } from './shared/types';
+import { SYSTEM_INSTRUCTION } from './shared/prompts';
+
+type TryCatchRequestParams =
+  | { isInitialQuestion: boolean; userAnswer?: string }
+  | { isInitialQuestion?: boolean; userAnswer: string };
 
 @Component({
   selector: 'tndm-ai-exam',
@@ -14,15 +20,69 @@ export class TndmAiExam {
   private readonly ollama = inject(AiExamOllamaService);
   readonly currentQuestion = signal('');
   readonly isLoading = signal(false);
+  readonly isGenerateQuestionDisabled = signal(false);
+  readonly isAnswerQuestionDisabled = signal(true);
+
+  private readonly chatHistory: Message[] = [{ role: ROLES.system, content: SYSTEM_INSTRUCTION }];
+  private readonly initialQuestion = `Ask me a question on JavaScript`;
 
   async generateQuestion(): Promise<void> {
     if (this.isLoading()) {
       return;
     }
+
+    this.tryCatchRequest({ isInitialQuestion: true });
+    this.isGenerateQuestionDisabled.set(true);
+  }
+
+  async answerQuestion(event: Event): Promise<void> {
+    event.preventDefault();
+    const form = event.target;
+
+    if (!(form instanceof HTMLFormElement)) {
+      throw new Error('HTMLFormElement expected');
+    }
+
+    const userAnswer = String(new FormData(form).get('user-answer'));
+    this.tryCatchRequest({ userAnswer });
+  }
+
+  private async tryCatchRequest({ isInitialQuestion, userAnswer }: TryCatchRequestParams): Promise<void> {
     this.isLoading.set(true);
 
-    const answer = await this.ollama.ask(`I'm ready. Ask me a question on JavaScript`);
-    this.currentQuestion.set(answer);
-    this.isLoading.set(false);
+    let answerFromAi = null;
+
+    try {
+      if (isInitialQuestion) {
+        answerFromAi = await this.ollama.ask(this.initialQuestion, this.chatHistory);
+        this.updateChatHistory(this.initialQuestion, answerFromAi);
+        this.isAnswerQuestionDisabled.set(false);
+      } else {
+        if (!userAnswer) {
+          throw new Error('Message content is NOT provided');
+        }
+        answerFromAi = await this.ollama.ask(userAnswer, this.chatHistory);
+        this.updateChatHistory(userAnswer, answerFromAi);
+      }
+
+      this.currentQuestion.set(answerFromAi);
+    } catch (error) {
+      console.error(`Failed to connect to the AI tutor.`, error);
+      this.currentQuestion.set('FAILED to connect to the AI tutor.');
+    } finally {
+      this.isLoading.set(false);
+      // console.log(this.chatHistory);
+    }
+  }
+
+  private updateChatHistory(questionToAi: string, answerFromAi: string): void {
+    this.chatHistory.push({
+      role: ROLES.user,
+      content: questionToAi,
+    });
+    this.chatHistory.push({
+      role: ROLES.assistant,
+      content: answerFromAi,
+    });
   }
 }
--- src/app/pages/games/ai-exam/ollama.service.ts
+++ src/app/pages/games/ai-exam/ollama.service.ts
@@ -1,35 +1,15 @@
 import { inject, Injectable } from '@angular/core';
 import { OllamaService } from '../../../core/ollama/ollama-service';
-
-const ROLES = {
-  user: 'user',
-  assistant: 'assistant',
-  system: 'system',
-} as const;
+import { Message, ROLES } from './shared/types';
 
 @Injectable()
 export class AiExamOllamaService {
   private readonly ollama = inject(OllamaService).client;
 
-  private readonly systemInstructions = `
-You are a JavaScript Tutor. Your goal is to help the user pass a JavaScript exam.
-RULES:
-1. Ask ONE technical JavaScript question at a time.
-2. OUTPUT ONLY THE QUESTION. Do not say "Great," "Here is your challenge," or "Hello."
-3. No conversational filler. Just the technical content.
-4. Rate every answer from 0% to 100%.
-5. Do NOT give the full answer. If the answer is wrong or incomplete, ask guiding questions to lead them to the truth.
-6. The user must reach 90% accuracy through their own reasoning.
-7. When they hit 90%+, you MUST say: 'You passed! Congrats!'
-`;
-
-  async ask(question: string): Promise<string> {
+  async ask(newQuestion: string, chatHistory: Message[]): Promise<string> {
     const response = await this.ollama.chat({
       model: 'deepseek-r1:8b',
-      messages: [
-        { role: ROLES.system, content: this.systemInstructions },
-        { role: ROLES.user, content: question },
-      ],
+      messages: [...chatHistory, { role: ROLES.user, content: newQuestion }],
     });
     return response.message.content;
   }
