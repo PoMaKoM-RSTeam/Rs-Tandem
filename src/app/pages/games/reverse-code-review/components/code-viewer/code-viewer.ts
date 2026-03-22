e416c8d3589977dd1ea70a86c595f9d9658e791a
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-03-22 10:42:06 +0500

    refactor: replace ::ng-deep with incapsulation
    style: edit styles according to changes, also replace some missed hardcoded values

--- src/app/pages/games/reverse-code-review/components/code-viewer/code-viewer.ts
+++ src/app/pages/games/reverse-code-review/components/code-viewer/code-viewer.ts
@@ -1,10 +1,12 @@
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, ViewEncapsulation } from '@angular/core';
 import { ReviewCase } from '../../models/review-case.model';
 import { highlightCode } from '../../helpers/highlight-code';
 
 @Component({
   selector: 'tndm-code-viewer',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+  host: { class: 'code-viewer' },
   templateUrl: './code-viewer.html',
   styleUrl: './code-viewer.scss',
 })
