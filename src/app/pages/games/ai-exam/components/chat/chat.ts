05adfcfdf686e461b0988309d35dde1dd705b8c1
mark-pribylnov <mark.pribylnov@gmail.com>
2026-03-23 17:13:37 +0300

    feat: move chat window to separate component

---
+++ src/app/pages/games/ai-exam/components/chat/chat.ts
@@ -0,0 +1,35 @@
+import { ChangeDetectionStrategy, Component, ElementRef, signal, viewChild } from '@angular/core';
+import { MarkdownComponent } from 'ngx-markdown';
+import { Message, ROLES } from '../../shared/types';
+import { SYSTEM_INSTRUCTION } from '../../shared/prompts';
+
+@Component({
+  selector: 'tndm-chat',
+  templateUrl: './chat.html',
+  imports: [MarkdownComponent],
+  styleUrl: './chat.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TndmChat {
+  readonly chatContainer = viewChild<ElementRef<HTMLDivElement>>('chatContainer');
+  readonly ROLES = ROLES;
+
+  readonly allMessages = signal<Message[]>([{ role: ROLES.system, content: SYSTEM_INSTRUCTION }]);
+  private readonly initialQuestion = `Ask me a question on JavaScript`;
+
+  updateChatHistory({ role, content }: Message): void {
+    const newMessage = { role, content };
+    this.allMessages.update(messages => [...messages, newMessage]);
+    this.scrollChatToBottom();
+  }
+
+  private scrollChatToBottom(): void {
+    requestAnimationFrame(() => {
+      const container = this.chatContainer()?.nativeElement;
+      if (!container) {
+        return;
+      }
+      container.scrollTop = container.scrollHeight;
+    });
+  }
+}
