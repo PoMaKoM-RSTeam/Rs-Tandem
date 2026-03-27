0705762eb55498da3c23bf27c55babb32b6aca5c
Alexandr Zhdanko <lex.zhdanko@gmail.com>
2026-03-27 20:33:04 +0400

    refactor: remove duplication in navigation component

--- src/app/shared/components/tndm-navigation/tndm-navigation.ts
+++ src/app/shared/components/tndm-navigation/tndm-navigation.ts
@@ -10,5 +10,11 @@ import { APP_ROUTES } from '../../constants/app-routes';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TndmNavigation {
-  readonly appRoutes = APP_ROUTES;
+  readonly navItems = [
+    { label: 'code_golf', path: APP_ROUTES.codeGolf },
+    { label: 'async_sorter', path: APP_ROUTES.asyncSorter },
+    { label: 'type_investigator', path: APP_ROUTES.typeInvestigator },
+    { label: 'sandbox', path: APP_ROUTES.sandbox },
+    { label: 'event_loop', path: APP_ROUTES.eventLoop },
+  ];
 }
