e034f48c48da24a56b220507b8549cd72036f5a7
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-03-30 23:07:24 +0500

    refactor: implement i18n into not-found

--- src/app/pages/tndm-not-found-page/tndm-not-found-page.ts
+++ src/app/pages/tndm-not-found-page/tndm-not-found-page.ts
@@ -2,19 +2,24 @@ import { ChangeDetectionStrategy, Component, computed, inject, Signal } from '@a
 import { ButtonConfig, TndmButton } from '../../shared/ui/tndm-button/tndm-button';
 import { DotGrid } from '../../shared/components/dot-grid/dot-grid';
 import { Router } from '@angular/router';
+import { TranslocoPipe, TranslocoService } from '@jsverse/transloco';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'tndm-tndm-not-found-page',
-  imports: [TndmButton, DotGrid],
+  imports: [TndmButton, DotGrid, TranslocoPipe],
   templateUrl: './tndm-not-found-page.html',
   styleUrl: './tndm-not-found-page.scss',
 })
 export class TndmNotFoundPage {
   private router: Router = inject(Router);
+  readonly translocoService = inject(TranslocoService);
+
+  readonly translatedButton = toSignal(this.translocoService.selectTranslate('nfound.mainbtn'));
 
   protected readonly toMainBtnConfig: Signal<ButtonConfig> = computed(() => ({
-    label: 'go to the main page',
+    label: this.translatedButton() ?? this.translocoService.translate('nfound.mainbtn'),
     type: 'submit',
   }));
 
