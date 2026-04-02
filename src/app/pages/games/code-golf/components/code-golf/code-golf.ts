e7b4a7032699200cdabfdae3114f6d5093545507
aliakseidrob <aliakseidrob@gmail.com>
2026-04-02 20:22:59 +0300

    fix: rebase main

--- src/app/pages/games/code-golf/components/code-golf/code-golf.ts
+++ src/app/pages/games/code-golf/components/code-golf/code-golf.ts
@@ -1,19 +1,18 @@
 import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
-
 import { TndmCodeGolfEditor } from '../code-golf-editor/code-golf-editor';
 import { TndmCodeGolfRank } from '../code-golf-rank/code-golf-rank';
 import { TndmButton } from '../../../../../shared/ui/tndm-button/tndm-button';
-
 import { TndmCodeGolfResults } from '../results-modal/results-modal';
-
 import { CodeGolfService } from '../../services/code-golf.service';
 import { TranslocoPipe, TranslocoService } from '@jsverse/transloco';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { CodeGolfFetcherService } from '../../services/code-golf-fetcher.service';
 
 @Component({
   selector: 'tndm-code-golf',
   standalone: true,
   imports: [TndmCodeGolfEditor, TndmCodeGolfRank, TndmButton, TndmCodeGolfResults, TranslocoPipe],
+  providers: [CodeGolfService, CodeGolfFetcherService],
   templateUrl: 'code-golf.html',
   styleUrl: './code-golf.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
