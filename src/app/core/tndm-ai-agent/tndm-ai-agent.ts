dc29fb9390b62cb1c8f09bbc49865e414ecb6557
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-04-05 01:25:18 +0300

    feat: add tndm-ai-agent ts file

---
+++ src/app/core/tndm-ai-agent/tndm-ai-agent.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { TndmAiAgentButton } from './tndm-ai-agent-button/tndm-ai-agent-button';
+import { TndmAiAgentWrap } from './tndm-ai-agent-wrap/tndm-ai-agent-wrap';
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'tndm-ai-agent',
+  imports: [TndmAiAgentButton, TndmAiAgentWrap],
+  templateUrl: './tndm-ai-agent.html',
+  styleUrl: './tndm-ai-agent.scss',
+  standalone: true,
+})
+export class TndmAiAgent {
+  protected readonly isOpen = signal(false);
+
+  toggleAgentWrap(): void {
+    this.isOpen.update(value => !value);
+  }
+}
