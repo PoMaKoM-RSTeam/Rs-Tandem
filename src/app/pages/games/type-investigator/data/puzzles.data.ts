deede03bc39c4c5a3c2981fdd5faee7452e038bf
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-04-06 20:01:38 +0500

    fix: fix small amount of puzzles
    add some (that's temporary puzzles)

--- src/app/pages/games/type-investigator/data/puzzles.data.ts
+++ src/app/pages/games/type-investigator/data/puzzles.data.ts
@@ -2,5 +2,7 @@ import { Puzzle } from '../models/puzzle.model';
 import { DATA_ONE } from './data-one';
 import { DATA_TWO } from './data-two';
 import { DATA_THREE } from './data-three';
+import { DATA_FOUR } from './data-four';
+import { DATA_FIVE } from './data-five';
 
-export const PUZZLES_DATA: Puzzle[] = [...DATA_ONE, ...DATA_TWO, ...DATA_THREE];
+export const PUZZLES_DATA: Puzzle[] = [...DATA_ONE, ...DATA_TWO, ...DATA_THREE, ...DATA_FOUR, ...DATA_FIVE];
