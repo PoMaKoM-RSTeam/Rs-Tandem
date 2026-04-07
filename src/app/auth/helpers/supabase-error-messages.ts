2864d4418798c221da5f0d21bf0118624e8ccae8
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-04-07 18:06:07 +0300

    feat: replace messages with their keys

--- src/app/auth/helpers/supabase-error-messages.ts
+++ src/app/auth/helpers/supabase-error-messages.ts
@@ -2,22 +2,22 @@ import { AuthError } from '@supabase/supabase-js';
 import { AUTH_ERROR_KEYS } from '../enums/auth-error-key';
 
 export const handleSupabaseAuthError = (error: AuthError): string => {
-  const message: string = error.message.toLowerCase();
+  const message = error.message.toLowerCase();
 
   if (message.includes(AUTH_ERROR_KEYS.InvalidCredentials)) {
-    return 'Неверный email или пароль'; //t(key) Hint for translations Wrong email or password
+    return 'auth.invalidCredentials';
   }
 
   if (message.includes(AUTH_ERROR_KEYS.UserAlreadyExists)) {
-    return 'Пользователь с таким email уже существует'; // t(key) User with this email already exists
+    return 'auth.userAlreadyExists';
   }
 
   if (message.includes(AUTH_ERROR_KEYS.EmailNotConfirmed)) {
-    return 'Подтвердите email перед входом'; // t(key) Please confirm your email before logging in
+    return 'auth.emailNotConfirmed';
   }
 
   if (message.includes(AUTH_ERROR_KEYS.WeakPassword)) {
-    return 'Слишком простой пароль'; // t(key) Password is too weak
+    return 'auth.weakPassword';
   }
 
   return error.message;
