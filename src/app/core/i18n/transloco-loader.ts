d28ad4397f518fb5c0fa519eae80dd45ae4c2acc
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-04-08 08:47:56 +0500

    fix: add timeout into loadFromSupabase method
    thanks to Anatoly! yesterday our conversation gave me some thoughts
    *todo - refactor loadFromSupabase: I guess, timeout not solves the problem
    so, i should move loadFromSupabase out from loader and make it a helper function
    because now it loading all the rules + json, perhaps it's worth to load separatly

--- src/app/core/i18n/transloco-loader.ts
+++ src/app/core/i18n/transloco-loader.ts
@@ -4,9 +4,10 @@ import { HttpClient } from '@angular/common/http';
 import { catchError, forkJoin, map, Observable, of } from 'rxjs';
 import { SupabaseService } from '../supabase/supabase-service';
 import { SupabaseClient } from '@supabase/supabase-js';
-import { from } from 'rxjs';
+import { from, timeout } from 'rxjs';
 
 const TRANSLATIONS_TABLE = 'rules';
+const SUPABASE_TIMEOUT = 5000;
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
@@ -14,7 +15,7 @@ export class TranslocoHttpLoader implements TranslocoLoader {
   private readonly supabase: SupabaseClient = inject(SupabaseService).client;
 
   getTranslation(lang: string): Observable<Translation> {
-    return forkJoin([this.loadFromJson(lang), this.loadFromSupabase(TRANSLATIONS_TABLE, lang)]).pipe(
+    return forkJoin([this.loadFromJson(lang), this.loadFromSupabase(TRANSLATIONS_TABLE, lang, SUPABASE_TIMEOUT)]).pipe(
       map(([jsonTranslations, supabaseTranslations]) => ({
         ...jsonTranslations,
         ...supabaseTranslations,
@@ -26,8 +27,9 @@ export class TranslocoHttpLoader implements TranslocoLoader {
     return this.http.get<Translation>(`/i18n/${lang}.json`);
   }
 
-  private loadFromSupabase(table: string, lang: string): Observable<Translation> {
+  private loadFromSupabase(table: string, lang: string, timeoutTime: number): Observable<Translation> {
     return from(this.supabase.from(table).select('key, value').eq('lang', lang)).pipe(
+      timeout(timeoutTime),
       map(({ data, error }) => {
         if (error || !data) return {};
         const translations: Translation = {};
