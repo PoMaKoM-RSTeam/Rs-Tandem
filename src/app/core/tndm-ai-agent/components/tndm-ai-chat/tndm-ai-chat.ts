cb2bdfe5a9e73b23b6a7385d33f64938df75c2a0
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-04-25 12:42:43 +0300

    feat: add ai-chat component

--- src/app/core/tndm-ai-agent/components/tndm-ai-chat/tndm-ai-chat.ts
+++ src/app/core/tndm-ai-agent/components/tndm-ai-chat/tndm-ai-chat.ts
@@ -1,6 +1,18 @@
-import { ChangeDetectionStrategy, Component, effect, ElementRef, inject, viewChild } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  effect,
+  ElementRef,
+  inject,
+  Signal,
+  viewChild,
+} from '@angular/core';
 import { TndmAiChatService } from '../../services/tndm-ai-chat-service';
 import { TndmAiChatMessage } from '../tndm-ai-chat-message/tndm-ai-chat-message';
+import { SvgIconComponent } from 'angular-svg-icon';
+import { ButtonConfig, TndmButton } from '../../../../shared/ui/tndm-button/tndm-button';
+import { translateSignal, TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'tndm-ai-chat',
@@ -8,7 +20,7 @@ import { TndmAiChatMessage } from '../tndm-ai-chat-message/tndm-ai-chat-message'
   templateUrl: './tndm-ai-chat.html',
   styleUrl: './tndm-ai-chat.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [TndmAiChatMessage],
+  imports: [TndmAiChatMessage, SvgIconComponent, TndmButton, TranslocoPipe],
 })
 export class TndmAiChat {
   private readonly chatService = inject(TndmAiChatService);
@@ -17,16 +29,31 @@ export class TndmAiChat {
   protected readonly assistantTyping = this.chatService.assistantTyping;
   protected readonly currentAssistantId = this.chatService.currentAssistantId;
 
-  private readonly scrollContainer = viewChild.required<ElementRef<HTMLDivElement>>('scrollContainer');
+  protected readonly clearBtnLabel = translateSignal('ai-agent.clear');
+  protected readonly clearBtnConfig: Signal<ButtonConfig> = computed(() => ({
+    variant: 'black',
+    icon: 'clear',
+    label: this.clearBtnLabel(),
+    isDisabled: this.chatService.assistantTyping(),
+  }));
+
+  private readonly scrollContainer = viewChild<ElementRef<HTMLDivElement>>('scrollContainer');
 
   constructor() {
     effect(() => {
       this.messages();
 
       queueMicrotask(() => {
-        const el = this.scrollContainer().nativeElement;
+        const elRef = this.scrollContainer();
+        if (!elRef) return;
+
+        const el = elRef.nativeElement;
         el.scrollTop = el.scrollHeight;
       });
     });
+  }
+
+  clearMessages(): void {
+    this.chatService.clearMessages();
   }
 }
